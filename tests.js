d42be3ba49bacc9cc0925475ded8093ffa676361
Lucas Vinn <90799507+LucasVinn@users.noreply.github.com>
2022-06-14 21:59:44 -0300

    Add files via upload

---
+++ tests.js
@@ -0,0 +1,48 @@
+function languageTransfer() {
+    
+    const English = document.getElementById("optionEnglish")
+
+    if (document.getElementById("optionEnglish") === English) {
+        const phraseEnglish = 'Moiayei is just an illusion in our world'
+        document.getElementById("header").innerHTML= phraseEnglish;
+    }
+
+}
+
+function languageTransferDeutsch() {
+
+    const Deutsch = document.getElementById("optionDeutsch")
+
+    if (document.getElementById("optionDeutsch") === Deutsch) {
+        const phraseDeutsch = 'Moiayei ist nur eine Illusion in unserer Welt'
+        document.getElementById("header").innerHTML= phraseDeutsch
+    }
+
+}
+
+/*
+function languageTransfer() {
+
+    const Deutsch = document.getElementById("optionDeutsch")
+
+    if (document.getElementById("optionDeutsch") === Deutsch) {
+        const phraseDeutsch = 'Moiayei ist nur eine Illusion in unserer Welt'
+        document.getElementById("header").innerHTML= phraseDeutsch
+    }
+
+}
+
+
+
+
+function englishTransfer() {
+
+    const phraseEnglish = 'Moiayei is just an illusion in our world'
+    document.getElementById("header").innerHTML= phraseEnglish;
+    
+
+}
+*/
+
+const console = 'moaiye'
+console.log(console)
